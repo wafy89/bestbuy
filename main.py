c7e0e8cdc29337d2c2a1cd9454fcf6cfdaa303d7
wafy89 <wafi.amer.17@gmail.com>
2026-02-19 16:36:39 +0100

    added user interface

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+from products import Product
+from store import Store
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+product_list = [ Product("MacBook Air M2", price=1450, quantity=100),
+                 Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                 Product("Google Pixel 7", price=500, quantity=250)
+               ]
+best_buy = Store(product_list)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def show_menu():
+    print("""
+1. List all products in store
+2. Show total amount in store
+3. Make an order
+4. Quit
+    """)
+
+def start(store:Store):
+    while True:
+        show_menu()
+        user_input = input("Press choose a number")
+        if user_input in ["1", "2", "3", "4"]:
+            break
+
+    for product in store.get_all_products():
+        product.show()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main():
+    start(best_buy)
+
+
+if __name__ == "__main__":
+    main()
